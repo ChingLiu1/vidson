436f4bd26034a25a860191f347583ca50019569c
chingliu <chingliu12@gmail.com>
2018-11-06 16:30:00 +0300

    we embeded an google iframe map

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
-var express,hbs,port;
+var express,hbs,port,nodemailer,bodyParser;
 
 express = require('express');
+nodemailer = require('nodemailer');
+bodyParser = require('body-parser');
 
 var app = express();
 hbs = require('hbs');
@@ -12,6 +14,9 @@ hbs.registerPartials(__dirname + '/views/partials');
 app.set('views', 'views');
 app.set('view engine', 'hbs');
 
+//body parser middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended : false}));
 
 app.get('/', (req, res) => {
     
@@ -30,6 +35,44 @@ app.get('/about', (req, res) => {
 app.get('/contact', (req, res) => {
     res.render('contact.hbs');
 });
+
+app.post('/schedule', (req, res) => {
+   var output = `
+        <h1>${req.body.username}</h1>
+        <h3>${req.body.email}</h3>
+        <p>${req.body.message}<p>
+`;
+    
+    var transporter = nodemailer.createTransport({
+        host: 'smtp.ethereal.email',
+        port: 587,
+        auth: {
+            user: 's7dzpnftfbxpolii@ethereal.email',
+            pass: '9CCQndxDs8xAtbRM1N'
+        }
+        
+    });
+    
+    transporter.sendMail({
+        from: '"Fred Foo " <s7dzpnftfbxpolii@ethereal.email>',
+        to : "chingliu12@gmail.com",
+        subject : req.body.username,
+        html : output,
+    },(err, info) => {
+        if (err) {
+            res.render('contact', {
+                failed : "smtp server is down"
+            });
+        }else{
+            res.render('contact',{
+                success : "message sent"
+            });
+        }
+    });
+    
+});
+
+
 
 app.listen(port, () => {
     console.log('listenning locally');
